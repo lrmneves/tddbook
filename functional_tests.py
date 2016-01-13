4f66c784b9dea95ff8c3fb09e910dab22afe307b
Leonardo <lrmneves@poli.ufrj.br>
2016-01-13 17:00:51 -0500

    created first unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicity_wait(3)
+	def tearDown(self):
+		self.browser.quit()
 
-assert 'Django' in browser.title
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#Checking homepage of app
+		self.browser.get('http://localhost:8000')
+
+		#Check the title
+		self.assertIn('To-Do',self.browser.title) 
+		self.fail("Finish the test!")
+
+		#enter to-do list
+
+
+		#Read Chapters
+
+		#after enter, page updates and show 1: Read Chapters as an item
+
+		#there is a textbox inviting other itens
+
+		#enter Write Reading Summary
+
+		#page updates and show both itens
+
+		#look for unique url
+
+
+if __name__ == '__main__':
+	unittest.main(warnings="ignore")
